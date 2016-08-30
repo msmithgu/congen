2a2a9d842fac6f20865a8c3c27448fa00f1021fe
Mark Smith-Guerrero <msmithgu@gmail.com>
2016-08-29 20:23:57 -0400

    Find congen files

--- bin/congen.js
+++ bin/congen.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('congen output')
+console.log('congen started...')
+
+var glob = require('glob')
+
+// Find congen files
+glob(
+  '**/*.congen',
+  function (err, files) {
+    if (err) {
+      console.error('Error finding congen files: ', err)
+    } else {
+      console.log('Found congen files: ', files)
+    }
+  }
+)
